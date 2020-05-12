d7745f553d37c9e40975e0f70a5400c736212f81
EvaChudia <eva.chudina@yandex.ru>
2020-05-13 00:29:19 +0300

    o

---
+++ app.js
@@ -0,0 +1,83 @@
+let aboutWindow = document.querySelector('.about');
+let secondWindow = document.querySelector('.second-window');
+let close1Window = document.querySelector( '.close-1');
+let thirdWindow = document.querySelector('.third-window','third-window-text', 'third-window-img');
+let close2Window = document.querySelector( '.close-2');
+let fourthWindow = document.querySelector('.fourth-window','.fourth-window-text', '.fourth-window-img')
+let close3Window = document.querySelector( '.close-3');
+let fiveWindow = document.querySelector('.five-window','.five-window-text', '.five-window-img')
+let close4Window = document.querySelector( '.close-4');
+let sixWindow = document.querySelector('.six-window','.six-window-text', '.six-window-img')
+let close5Window = document.querySelector( '.close-5');
+let sevenWindow = document.querySelector('.seven-window','.seven-window-text', '.seven-window-img')
+let close6Window = document.querySelector( '.close-6');
+let eightWindow = document.querySelector('.eight-window','.eight-window-text', '.eight-window-img')
+let close7Window = document.querySelector( '.close-7');
+let nineWindow = document.querySelector('.nine-window','.nine-window-text', '.nine-window-img')
+let close8Window = document.querySelector( '.close-8');
+let tenWindow = document.querySelector('.ten-window','.ten-window-text', '.ten-window-img')
+let close9Window = document.querySelector( '.close-9');
+let elevenWindow = document.querySelector('.eleven-window','.eleven-window-text', '.eleven-window-img')
+let close10Window = document.querySelector( '.close-10');
+let twelfWindow = document.querySelector('.twelf-window','.twelf-window-text', '.twelf-window-img')
+let close11Window = document.querySelector( '.close-11');
+let thirteenWindow = document.querySelector('.thirteen-window','.thirteen-window-text', '.thirteen-window-img')
+let close12Window = document.querySelector( '.close-12');
+let fourteenWindow = document.querySelector('.fourteen-window','.fourteen-window-text', '.fourteen-window-img')
+let close13Window = document.querySelector( '.close-13');
+let fivteenWindow = document.querySelector('fivteen-window','.fivteen-window-text', '.fivteen-window-img')
+let close14Window = document.querySelector( '.close-14');
+let sixteenWindow = document.querySelector('sixteen-window','.sixteen-window-text', '.sixteen-window-img')
+let close15Window = document.querySelector( '.close-15');
+
+
+
+aboutWindow.addEventListener('click', function(){
+    secondWindow.style.setProperty('visibility', 'visible');
+});
+
+close1Window.addEventListener('click', function(){
+    thirdWindow.style.setProperty('visibility', 'visible');
+
+});
+close2Window.addEventListener('click', function(){
+    fourthWindow.style.setProperty('visibility', 'visible');
+});
+close3Window.addEventListener('click', function(){
+    fiveWindow.style.setProperty('visibility', 'visible');
+});
+close4Window.addEventListener('click', function(){
+    sixWindow.style.setProperty('visibility', 'visible');
+});
+close5Window.addEventListener('click', function(){
+    sevenWindow.style.setProperty('visibility', 'visible');
+});
+close6Window.addEventListener('click', function(){
+    eightWindow.style.setProperty('visibility', 'visible');
+});
+close7Window.addEventListener('click', function(){
+    nineWindow.style.setProperty('visibility', 'visible');
+});
+close8Window.addEventListener('click', function(){
+    tenWindow.style.setProperty('visibility', 'visible');
+});
+close9Window.addEventListener('click', function(){
+    elevenWindow.style.setProperty('visibility', 'visible');
+});
+close10Window.addEventListener('click', function(){
+    twelfWindow.style.setProperty('visibility', 'visible');
+});
+close11Window.addEventListener('click', function(){
+    thirteenWindow.style.setProperty('visibility', 'visible');
+});
+close12Window.addEventListener('click', function(){
+    fourteenWindow.style.setProperty('visibility', 'visible');
+});
+close13Window.addEventListener('click', function(){
+    fivteenWindow.style.setProperty('visibility', 'visible');
+});
+close14Window.addEventListener('click', function(){
+    sixteenWindow.style.setProperty('visibility', 'visible');
+});
+
+
